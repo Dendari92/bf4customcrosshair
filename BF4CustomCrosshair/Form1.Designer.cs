5f262aad8deac1efb3c7c87e41c5f8a2396d43d3
Dendari <dendari@hotmail.it>
2015-04-02 13:30:07 +0200

    Initial commit

---
+++ BF4CustomCrosshair/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace BF4CustomCrosshair
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Size";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.LargeChange = 10;
+            this.trackBar1.Location = new System.Drawing.Point(12, 9);
+            this.trackBar1.Maximum = 150;
+            this.trackBar1.Minimum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(75, 25);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.AutoSize = false;
+            this.trackBar2.LargeChange = 10;
+            this.trackBar2.Location = new System.Drawing.Point(12, 40);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(75, 25);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.TickFrequency = 10;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Visibility";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Color (RGB)";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.AutoSize = false;
+            this.trackBar3.LargeChange = 25;
+            this.trackBar3.Location = new System.Drawing.Point(12, 117);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(75, 25);
+            this.trackBar3.TabIndex = 6;
+            this.trackBar3.TickFrequency = 10;
+            this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.AutoSize = false;
+            this.trackBar4.LargeChange = 25;
+            this.trackBar4.Location = new System.Drawing.Point(93, 117);
+            this.trackBar4.Maximum = 255;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(75, 25);
+            this.trackBar4.TabIndex = 7;
+            this.trackBar4.TickFrequency = 10;
+            this.trackBar4.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.AutoSize = false;
+            this.trackBar5.LargeChange = 25;
+            this.trackBar5.Location = new System.Drawing.Point(174, 117);
+            this.trackBar5.Maximum = 255;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(75, 25);
+            this.trackBar5.TabIndex = 8;
+            this.trackBar5.TickFrequency = 10;
+            this.trackBar5.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(160, -2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(87, 77);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "+";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 181);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Crosshair Custom";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BF4CustomCrosshair/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BF4CustomCrosshair
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(trackBar1, trackBar1.Value + "");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            //
+            Char nul = (char)0;
+            Char tab = (char)9;
+            //
+            //
+            String XhairSize = "CrosshairSize" + nul + tab + nul + nul + nul;
+            String XhairVisibility = "CrosshairVisibility" + nul + tab + nul + nul + nul;
+            String XhairColorR = "CrosshairColorR" + nul + tab + nul + nul + nul;
+            String XhairColorG = "CrosshairColorG" + nul + tab + nul + nul + nul;
+            String XhairColorB = "CrosshairColorB" + nul + tab + nul + nul + nul;
+            //
+            String settingFile = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Battlefield 4\\settings\\PROFSAVE";
+            using (StreamReader fileReader = new StreamReader(settingFile))
+            {
+                String line = fileReader.ReadToEnd();
+                if (line.Contains(XhairSize))
+                {
+                    String newLine = line.Substring(line.IndexOf(XhairSize) + 18, 8).Replace('.', ',');
+                    trackBar1.Value = Convert.ToInt16(Convert.ToDecimal(newLine)) * 100;
+                }  
+                if (line.Contains(XhairVisibility))
+                {
+                    String newLine = line.Substring(line.IndexOf(XhairVisibility) + 24, 8).Replace('.', ',');
+                    trackBar2.Value = Convert.ToInt16(Convert.ToDecimal(newLine)) * 100;
+                }
+                if (line.Contains(XhairColorR))
+                {
+                    String newLine = line.Substring(line.IndexOf(XhairColorR) + 20, 8).Replace('.', ',');                
+                    trackBar3.Value = Convert.ToInt16(((Convert.ToDecimal(newLine) * 100) * 255) / 100);
+                }
+                if (line.Contains(XhairColorG))
+                {
+                    String newLine = line.Substring(line.IndexOf(XhairColorG) + 20, 8).Replace('.', ',');
+                    trackBar4.Value = Convert.ToInt16(((Convert.ToDecimal(newLine) * 100) * 255) / 100);
+                }
+                if (line.Contains(XhairColorB))
+                {
+                    String newLine = line.Substring(line.IndexOf(XhairColorB) + 20, 8).Replace('.', ',');
+                    trackBar5.Value = Convert.ToInt16(((Convert.ToDecimal(newLine) * 100) * 255) / 100);
+                }
+            }
+            //
+            toolTip1.SetToolTip(trackBar1, trackBar1.Value + "");
+            toolTip1.SetToolTip(trackBar2, trackBar2.Value + "");
+            toolTip1.SetToolTip(trackBar3, trackBar3.Value + "");
+            toolTip1.SetToolTip(trackBar4, trackBar4.Value + "");
+            toolTip1.SetToolTip(trackBar5, trackBar5.Value + "");
+            textBox1.ForeColor = Color.FromArgb(trackBar3.Value, trackBar4.Value, trackBar5.Value);
+            //
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(trackBar2, trackBar2.Value + "");
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(trackBar3, trackBar3.Value + "");
+            textBox1.ForeColor = Color.FromArgb(trackBar3.Value, trackBar4.Value, trackBar5.Value);
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(trackBar4, trackBar4.Value + "");
+            textBox1.ForeColor = Color.FromArgb(trackBar3.Value, trackBar4.Value, trackBar5.Value);
+        }
+
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(trackBar5, trackBar5.Value + "");
+            textBox1.ForeColor = Color.FromArgb(trackBar3.Value, trackBar4.Value, trackBar5.Value);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //
+            String XhairSize = ("GstGameplay.CrosshairSize " + (Convert.ToDecimal(trackBar1.Value) / 100));
+            String XhairVisibility = ("GstGameplay.CrosshairVisibility " + (Convert.ToDecimal(trackBar2.Value) / 100));
+            String XhairColorR = ("GstGameplay.CrosshairColorR " + ((((Convert.ToDecimal(trackBar3.Value) / 100) / 255) * 100).ToString("F")));
+            String XhairColorG = ("GstGameplay.CrosshairColorG " + ((((Convert.ToDecimal(trackBar4.Value) / 100) / 255) * 100).ToString("F")));
+            String XhairColorB = ("GstGameplay.CrosshairColorB " + ((((Convert.ToDecimal(trackBar5.Value) / 100) / 255) * 100).ToString("F")));
+            //
+            //
+            int checkSize = 0;
+            int checkVisibility = 0;
+            int checkColorR = 0;
+            int checkColorG = 0;
+            int checkColorB = 0;
+            //
+            //
+            String settingFile = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Battlefield 4\\settings\\PROFSAVE_profile";
+            using (StreamReader fileReader = new StreamReader(settingFile))
+            {
+                String line = fileReader.ReadToEnd();
+                if (line.Contains(XhairSize.Replace(',', '.')))
+                {
+                    checkSize = 1;
+                }
+                if (line.Contains(XhairVisibility.Replace(',', '.')))
+                {
+                    checkVisibility = 1;
+                }
+                if (line.Contains(XhairColorR.Replace(',', '.')))
+                {
+                    checkColorR = 1;
+                }
+                if (line.Contains(XhairColorG.Replace(',', '.')))
+                {
+                    checkColorG = 1;
+                }
+                if (line.Contains(XhairColorB.Replace(',', '.')))
+                {
+                    checkColorB = 1;
+                }
+            }
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(settingFile, FileMode.Append);
+                using (StreamWriter fileWriter = new StreamWriter(fs))
+                {
+                    
+                    if (checkSize == 0)
+                    {
+                        fileWriter.WriteLine();
+                        fileWriter.Write(XhairSize.Replace(',', '.'));
+                    } 
+                    if (checkVisibility == 0)
+                    {
+                        fileWriter.WriteLine();
+                        fileWriter.Write(XhairVisibility.Replace(',', '.'));
+                    }
+                    if (checkColorR == 0)
+                    {
+                        fileWriter.WriteLine();
+                        fileWriter.Write(XhairColorR.Replace(',', '.'));
+                    }
+                    if (checkColorG == 0)
+                    {
+                        fileWriter.WriteLine();
+                        fileWriter.Write(XhairColorG.Replace(',', '.'));
+                    }
+                    if (checkColorB == 0)
+                    {
+                        fileWriter.WriteLine();
+                        fileWriter.Write(XhairColorB.Replace(',', '.'));
+                    }
+                }
+            }
+            finally
+            {
+                if (fs != null)
+                    fs.Dispose();
+            }
+            MessageBox.Show("Operation complete!", "SUCCESS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
